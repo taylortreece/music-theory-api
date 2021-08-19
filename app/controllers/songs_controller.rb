7672a2554225c8be75f2126a1af6927e1da8ab9c
taylortreece <tayloratreece@gmail.com>
2021-08-19 00:25:51 -0500

    fixed json error by adding json renderings to Create, Update, and Delete methods in song controller.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -20,24 +20,72 @@ class SongsController < ApplicationController
     end
 
     def create
-        Song.create_song_from_frontend(params)
+        song = Song.create_song_from_frontend(params)
+        render json: song.to_json(
+            :include => [:scales, :chords,
+                :keys => {
+                    :include => {
+                        :scales => {include: [
+                            :notes, 
+                            :chords => {include: [:notes]}
+                        ]}
+                    }
+                }
+            ]
+        )
     end
+
+    # def Song.create_song_from_frontend(params)
+    #     chords = params[:chords].map{ |chord| Chord.find_by(name: chord[:name])}
+    #     song = Song.create(
+    #         name: params[:song_name], 
+    #         keys: [Key.find_by(name: params[:key_name], key_type: params[:key_type])], 
+    #         chords: chords,
+    #         scales: [Scale.find_by(name: params[:song_scale])]
+    #     )
+    # end
 
     def update
         song = Song.find_by(name: params[:name])
         chords = params[:chords].map{|chord| Chord.find_by(name: chord[:name])}
         song.update(chords: chords)
+        render json: song.to_json(
+            :include => [:scales, :chords,
+                :keys => {
+                    :include => {
+                        :scales => {include: [
+                            :notes, 
+                            :chords => {include: [:notes]}
+                        ]}
+                    }
+                }
+            ]
+        )
     end
 
     def delete
-        Song.find_by(name: params[:name]).delete
+        song = Song.find_by(name: params[:name])
+        render json: song.to_json(
+            :include => [:scales, :chords,
+                :keys => {
+                    :include => {
+                        :scales => {include: [
+                            :notes, 
+                            :chords => {include: [:notes]}
+                        ]}
+                    }
+                }
+            ]
+        )
+
+        song.delete
     end
 
-    private 
+    # private 
 
-    def song_params
-        params.require(:song).permit(:name, :key, :chords)
-    end
+    # def song_params
+    #     params.require(:song).permit(:name, :key, :chords)
+    # end
     
     # def self.create_song_from_frontend(params)
     #     chords = params[:chords].map{ |chord| Chord.find_by(name: chord[:name])}
